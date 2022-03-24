a2b75fb85fa501842f02a69586c980713e601e33
Jan Steuer <jan.steuer@centrum.cz>
2022-03-24 10:23:06 +0100

    Ignore null values within response

--- src/main/kotlin/cz/cvut/fit/steuejan/travel/api/app/Application.kt
+++ src/main/kotlin/cz/cvut/fit/steuejan/travel/api/app/Application.kt
@@ -4,6 +4,7 @@ import cz.cvut.fit.steuejan.travel.api.app.plugin.*
 import cz.cvut.fit.steuejan.travel.data.config.Hikari
 import cz.cvut.fit.steuejan.travel.data.database.initDatabase
 import io.ktor.application.*
+import kotlinx.serialization.ExperimentalSerializationApi
 import org.koin.ktor.ext.inject
 
 const val baseUrl = "http://localhost:8080"
@@ -11,6 +12,7 @@ const val baseUrl = "http://localhost:8080"
 fun main(args: Array<String>): Unit =
     io.ktor.server.netty.EngineMain.main(args)
 
+@ExperimentalSerializationApi
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
     configureDI()
--- src/main/kotlin/cz/cvut/fit/steuejan/travel/api/app/plugin/Serialization.kt
+++ src/main/kotlin/cz/cvut/fit/steuejan/travel/api/app/plugin/Serialization.kt
@@ -3,13 +3,16 @@ package cz.cvut.fit.steuejan.travel.api.app.plugin
 import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.serialization.*
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 
+@ExperimentalSerializationApi
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
         json(json = Json {
             prettyPrint = true
             ignoreUnknownKeys = true
+            explicitNulls = false
         })
     }
 }
