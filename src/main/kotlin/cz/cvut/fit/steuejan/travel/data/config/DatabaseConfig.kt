1d04773521f8e528ab8926b662452073bc6d804a
Jan Steuer <jan.steuer@centrum.cz>
2022-04-11 19:31:47 +0200

    Establish jdbc connection from provided env variable by heroku

--- src/main/kotlin/cz/cvut/fit/steuejan/travel/data/config/DatabaseConfig.kt
+++ src/main/kotlin/cz/cvut/fit/steuejan/travel/data/config/DatabaseConfig.kt
@@ -4,19 +4,11 @@ import cz.cvut.fit.steuejan.travel.api.app.config.AppConfig
 import io.ktor.config.*
 
 class DatabaseConfig(override val config: ApplicationConfig) : AppConfig {
-    val host = config.property(HOST).getString()
-    val port = config.property(PORT).getString()
-    val name = config.property(NAME).getString()
-    val user = config.property(USER).getString()
-    val pass = config.property(PASSWORD).getString()
+    val databaseUri = config.property(DATABASE_URI).getString()
     val maxPoolSize = config.property(MAX_POOL_SIZE).getString().toInt()
 
     companion object {
-        private const val HOST = "database.host"
-        private const val PORT = "database.port"
-        private const val NAME = "database.name"
-        private const val USER = "database.user"
-        private const val PASSWORD = "database.password"
+        private const val DATABASE_URI = "database.uri"
         private const val MAX_POOL_SIZE = "database.maxPoolSize"
 
         const val NAME_LENGTH = 140
