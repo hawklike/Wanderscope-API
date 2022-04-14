8da85d8e2e990ad2bbd36f893a0355fe0d16fb6c
Jan Steuer <jan.steuer@centrum.cz>
2022-04-14 13:42:56 +0200

    Add date items among other POIs in itinerary

--- src/main/kotlin/cz/cvut/fit/steuejan/travel/api/trip/itinerary/controller/ItineraryController.kt
+++ src/main/kotlin/cz/cvut/fit/steuejan/travel/api/trip/itinerary/controller/ItineraryController.kt
@@ -3,7 +3,11 @@ package cz.cvut.fit.steuejan.travel.api.trip.itinerary.controller
 import cz.cvut.fit.steuejan.travel.api.app.di.factory.DaoFactory
 import cz.cvut.fit.steuejan.travel.api.app.response.Response
 import cz.cvut.fit.steuejan.travel.api.trip.controller.AbstractTripController
+import cz.cvut.fit.steuejan.travel.api.trip.itinerary.model.CommonItinerary
+import cz.cvut.fit.steuejan.travel.api.trip.itinerary.model.DateItinerary
 import cz.cvut.fit.steuejan.travel.api.trip.itinerary.response.TripItineraryResponse
+import cz.cvut.fit.steuejan.travel.data.model.Duration
+import org.joda.time.LocalDate
 
 class ItineraryController(daoFactory: DaoFactory) : AbstractTripController(daoFactory) {
 
@@ -17,6 +21,38 @@ class ItineraryController(daoFactory: DaoFactory) : AbstractTripController(daoFa
             transports + accommodation + activities + places
         }
         val itinerary = allPoints.sortedWith(compareBy(nullsLast()) { it.duration.startDate })
-        return TripItineraryResponse.success(itinerary)
+        val itineraryWithDates = addDateMarks(itinerary.toMutableList())
+        return TripItineraryResponse.success(itineraryWithDates)
+    }
+
+    private fun addDateMarks(itinerary: MutableList<CommonItinerary>): List<CommonItinerary> {
+        var lastDateModified = false
+        var lastDate: LocalDate? = null
+        val iterator = itinerary.listIterator()
+
+        //add date marks before each individual days of some POIs
+        for (item in iterator) {
+            val currentDate = item.duration.startDate?.toLocalDate()
+            if (currentDate != lastDate) {
+                val duration = Duration(startDate = item.duration.startDate)
+                iterator.addBefore(DateItinerary(duration))
+                lastDate = currentDate
+                lastDateModified = true
+            }
+        }
+
+        //itinerary is not empty, but none of the POIs have date set
+        //set date mark as empty
+        if (!lastDateModified && itinerary.isNotEmpty()) {
+            itinerary.add(0, DateItinerary(Duration()))
+        }
+
+        return itinerary
+    }
+
+    private fun <T> MutableListIterator<T>.addBefore(item: T) {
+        this.previous()
+        this.add(item)
+        this.next()
     }
 }
