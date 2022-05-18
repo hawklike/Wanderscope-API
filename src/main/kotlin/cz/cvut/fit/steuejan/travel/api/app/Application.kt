3820bbd79734781e3ef40ab9a2cae5d25b88d82e
Jan Steuer <jan.steuer@centrum.cz>
2022-05-18 11:24:20 +0200

    Close HttpClient when stopping the application

--- src/main/kotlin/cz/cvut/fit/steuejan/travel/api/app/Application.kt
+++ src/main/kotlin/cz/cvut/fit/steuejan/travel/api/app/Application.kt
@@ -4,6 +4,7 @@ import cz.cvut.fit.steuejan.travel.api.app.plugin.*
 import cz.cvut.fit.steuejan.travel.data.config.Hikari
 import cz.cvut.fit.steuejan.travel.data.database.initDatabase
 import io.ktor.application.*
+import io.ktor.client.*
 import kotlinx.serialization.ExperimentalSerializationApi
 import org.koin.ktor.ext.inject
 
@@ -13,6 +14,7 @@ fun main(args: Array<String>): Unit =
 @ExperimentalSerializationApi
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
+    monitore()
     configureDI()
     configureStatusPages()
     configureDataConversion()
@@ -27,4 +29,15 @@ fun Application.module() {
 fun Application.configureDB() {
     val hikari: Hikari by inject()
     initDatabase(hikari)
+}
+
+fun Application.monitore() {
+    environment.monitor.subscribe(ApplicationStopPreparing, ::onPrepareStop)
+}
+
+private fun Application.onPrepareStop(env: ApplicationEnvironment) {
+    val client: HttpClient by inject()
+    env.log.info("Closing all resources")
+    client.close()
+    env.log.info("HttpClient closed")
 }
