816a9c5b27fc5a31c7c2e4133d45f1d9f9a5061a
Jan Steuer <jan.steuer@centrum.cz>
2022-06-01 14:56:55 +0200

    Remove unique constraint fix

--- src/main/kotlin/cz/cvut/fit/steuejan/travel/data/database/expense/ExpenseRoomTable.kt
+++ src/main/kotlin/cz/cvut/fit/steuejan/travel/data/database/expense/ExpenseRoomTable.kt
@@ -6,7 +6,7 @@ import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.ReferenceOption.CASCADE
 
 object ExpenseRoomTable : IntIdTable("expense_rooms") {
-    val trip = reference("trip", TripTable, onDelete = CASCADE, onUpdate = CASCADE).uniqueIndex()
+    val trip = reference("trip", TripTable, onDelete = CASCADE, onUpdate = CASCADE)
 
     val name = varchar("name", DatabaseConfig.NAME_LENGTH)
     val currency = varchar("currency", DatabaseConfig.CURRENCY_CODE_LENGTH)
